d1b991029d8fe135ba357f81e663a1395c7f2985
jwywoo <jwywoo26@gmail.com>
2024-09-05 17:09:45 +0900

    [FEAT] #1 Project Setting

---
+++ src/main/java/com/example/awhombackend/AWhomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.awhombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AWhomBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AWhomBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/awhombackend/AWhomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awhombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AWhomBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
